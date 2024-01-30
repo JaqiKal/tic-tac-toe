12833fa9abcd652f68432975ab5b00cc2c621f26
Jacqueline Kalmár <jaqika@gmail.com>
2024-01-30 16:28:55 +0100

    Add index, form, css, script and library structure.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Tic-Tac-Toe game
+ * 
+ * This script manages the functionality of a Tic-Tac-Toe game in a web application.
+ * Players can take turns marking Xs and Os on a 3x3 grid with the objective of
+ * aligning three of their symbols in a row to win. The script handles player
+ * interactions, win conditions, and game resets.
+ *
+ * Features:
+ * - Player turns and symbol tracking (X and O)
+ * - Win condition checking for rows, columns, and diagonals
+ * - Detection of a draw (cat's game) when the board is full
+ * - Resetting the game for a new round
+ *
+ * Author: Jacqueline Kalmár
+ * Date: 2024 Feb 15
+ *
+ */ 
